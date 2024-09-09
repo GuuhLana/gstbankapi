e850f2cec47852c22165f40522f8e66428f3e42f
Gustavo-Notebook <gustavolr401@gmail.com>
2024-09-09 20:22:42 -0300

    Adicionando flyway ao projeto

--- src/main/resources/META-INF/DB/INSERT_DEFAULT_VALUES.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('01', '01', '1000', 'Beatriz Biagioni dos Santos');
-INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('01', '02', '1000', 'Erick de Sousa Cavalcante');
-INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('02', '01', '1000', 'Eduarda Ferreira Galeno');
-INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('02', '02', '1000', 'Gustavo de Lana Rocha');
-INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('03', '01', '1000', 'Julia Garcia Menezes');
-INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('03', '02', '1000', 'Ygor Brancaglione Cuchi');
---
+++ src/main/resources/db/migration/V1__INSERE_VALORES_DEFAULT.sql
@@ -0,0 +1,29 @@
+-- Insere valores iniciais para testar a API --
+
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('01', '03', '1000', 'Lucas Martins Silva');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('01', '04', '1000', 'Ana Paula Oliveira');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('02', '03', '1000', 'Marcos Vinícius Santos');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('02', '04', '1000', 'Larissa Costa Almeida');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('03', '03', '1000', 'Felipe Souza Lima');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('03', '04', '1000', 'Carla Beatriz Pereira');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('04', '01', '1000', 'Bruno Henrique Gomes');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('04', '02', '1000', 'Renata Melo Silva');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('04', '03', '1000', 'Ricardo Ramos Costa');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('05', '01', '1000', 'Gabriela Ferreira Lima');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('05', '02', '1000', 'Thiago da Silva Oliveira');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('05', '03', '1000', 'Mariana Barbosa Costa');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('06', '01', '1000', 'José Carlos Souza');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('06', '02', '1000', 'Amanda Ribeiro Rocha');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('06', '03', '1000', 'Fernando de Oliveira');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('07', '01', '1000', 'Paula Maria Fernandes');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('07', '02', '1000', 'Diego Batista Gomes');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('07', '03', '1000', 'Juliana Silva Santos');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('08', '01', '1000', 'Rodrigo Almeida Martins');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('08', '02', '1000', 'Vanessa Pereira Lopes');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('08', '03', '1000', 'Vinícius Andrade Souza');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('09', '01', '1000', 'Patrícia Martins Souza');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('09', '02', '1000', 'Daniel Ferreira Silva');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('09', '03', '1000', 'Isabela Santos Lima');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('10', '01', '1000', 'Fábio dos Santos Pereira');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('10', '02', '1000', 'Camila Oliveira Lima');
+INSERT INTO tb_conta(agencia, numero, saldo, titular) VALUES ('10', '03', '1000', 'João Pedro Ferreira');
